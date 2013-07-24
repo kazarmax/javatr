76963df962f4ce6d297b62ee1354bcb7040f78d9
Maksim Kazartsev <kazarmax@gmail.com>
2013-07-24 09:23:04 +0400

    Created two classes: Main and Field

---
+++ learningJava/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Field field = new Field();
+        field.eraseField();
+        field.showFields();
+
+    }
+
+}
+
