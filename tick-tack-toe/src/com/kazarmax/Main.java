013b945dc4ef7448acc3641227b270301dee8dc9
unknown <m.kazartsev@msk-note94.pyn.ru>
2013-08-02 17:51:01 +0400

    Added files Field.java and Main.java in the newly created Idea project

---
+++ tick-tack-toe/src/com/kazarmax/Main.java
@@ -0,0 +1,19 @@
+package com.kazarmax;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Field field = new Field();
+        field.eraseField();
+        field.showFields();
+
+        field = new Field(10);
+        field.eraseField();
+        field.showFields();
+
+
+    }
+
+}
